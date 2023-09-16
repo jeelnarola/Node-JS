decbc1c215594524445a8adc35c6862d06a7ea5a
jeel narola <narolajeel29@gmail.com>
2023-09-16 19:36:07 +0530

    Express GET,POST

---
+++ EXPRESS/GET,POST/index.js
@@ -0,0 +1,38 @@
+const express=require("express")
+const cors=require('cors')
+const app=express()
+app.use(express.json())
+app.use(cors())
+
+app.get('/',(req,res)=>{
+    res.send("Home Page !")
+})
+
+app.get('/login',(req,res)=>{
+    res.send("Login Page !")
+})
+
+
+app.post('/login', (req, res) => {
+    // const { username, password } = req.body;
+    res.status(200).json({ message: 'Login successful'});
+//     if (username === 'user' && password === 'password') {
+//       res.status(200).json({ message: 'Login successful' });
+//     } else {
+//       res.status(401).json({ message: 'Login failed' });
+//     }
+  });
+
+
+app.post("/sign",(req,res)=>{
+    console.log(req.body);
+    res.send(req.body)
+})
+
+app.post("/product",(req,res)=>{
+    console.log(req.body);
+    res.status(200).send(req.body)
+})
+
+
+app.listen(3000,()=>{console.log(`server satart`)})
