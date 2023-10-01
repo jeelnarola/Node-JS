36f6060dab31b29e87800219a76099d7172a8ed3
jeel narola <narolajeel29@gmail.com>
2023-10-01 11:41:19 +0530

    studnet query

--- Mongoose/database.js
+++ Mongoose/database.js
@@ -3,7 +3,7 @@ const mongoose=require('mongoose')
 
 const data=async()=>{
     await mongoose.connect('mongodb://127.0.0.1:27017')
-    console.log("Database connect in index js");
+    console.log("Database connect in index");
 }
 
 module.exports={data}
